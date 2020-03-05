b2e53582235e213d14aa547eb39c16d9177c23d6
GBTC440014UR <GBTC440014UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-03-04 20:21:48 -0500

    Initial commit

---
+++ src/main/java/com/ae/springboot201/HomeController.java
@@ -0,0 +1,12 @@
+package com.ae.springboot201;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/loadform")
+    public String loadFormPage(){
+        return "formtemplate";
+    }
+}
---
+++ src/test/java/com/ae/springboot201/Springboot201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ae.springboot201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
